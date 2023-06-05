d27ef0ffe2e9d23af299eeba66452925e38fc61e
Utsav <Utsav.Awasthi@pfizer.com>
2023-06-05 10:32:29 -0400

    Update pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -2,11 +2,11 @@
 ## Parameter estimation subject to uncertainity
 
 # Step 1: Indetify the model inputs, outputs, parameters(fixed, uncertain) (Design space)
-# Step 2: Validated candidate process model
+# Step 2: Validated candidate process model and intial data on experiments
 # Step 3: Uncertainity in the parameters
 # Step 4: Perform an intial senstivity analysis for the uncertainity to check model responses
     # Step 4a: Compute the model sensitivities
     # Step 4b: Packages scipy, CASaDi
-# Step 5: Calculate FIM,
+# Step 5: Calculate FIM using scaled sensitivity
     # Step 5a: Compute A-,D-,E- Optimiality criterion and also check other optimality criterion
-# Step 6: Plot of FIM, 
+# Step 6: Plot the optmality criterion, design vector space
