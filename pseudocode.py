5a078bb1212bb9c55770432ea9073a27c6e30305
Utsav <Utsav.Awasthi@pfizer.com>
2023-06-05 09:52:22 -0400

    Update pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -1,7 +1,12 @@
 # The main framework for MBDoE
 ## Parameter estimation subject to uncertainity
 
-# Step 1: Initial validated model of the process
-# Step 2: Determine the sensivities for the process
-#   Step 2a: Packages scipy, CASaDi
-# Step 3: 
+# Step 1: Indetify the model inputs, outputs, parameters(fixed, uncertain) (Design space)
+# Step 2: Validated candidate process model
+# Step 3: Uncertainity in the parameters
+# Step 4: Perform an intial senstivity analysis for the uncertainity to check model responses
+    # Step 4a: Compute the model sensitivities
+    # Step 4b: Packages scipy, CASaDi
+# Step 5: Calculate FIM,
+    # Step 5a: Compute A-,D-,E- Optimiality criterion and also check other optimality criterion
+# Step 6: Plot of FIM, 
